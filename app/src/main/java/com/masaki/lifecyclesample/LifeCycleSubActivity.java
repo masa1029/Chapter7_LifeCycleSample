fb86e6ea1accd1c88baee6f0f6e2dddd6d1e933a
masa1029 <masaco1290@gmail.com>
2018-06-06 11:28:56 +0900

    Initial commit

---
+++ app/src/main/java/com/masaki/lifecyclesample/LifeCycleSubActivity.java
@@ -0,0 +1,56 @@
+package com.masaki.lifecyclesample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class LifeCycleSubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("LifeCycleSample", "Sub onCreate() called");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_cycle_sub);
+    }
+    @Override
+    public void onStart() {
+        Log.i("LifeCycleSample", "Main onStart() called.");
+        super.onStart();
+    }
+
+    @Override
+    public void onRestart() {
+        Log.i("LifeCycleSample", "Main onRestart() called.");
+        super.onRestart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i("LifeCycleSample", "Main onResume() called.");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i("LifeCycleSample", "Main onPause() called.");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i("LifeCycleSample", "Main onStop() called.");
+        super.onStop();
+    }
+    @Override
+    public void onDestroy() {
+        Log.i("LifeCycleSample", "Main onDestroy() called.");
+        super.onDestroy();
+    }
+
+
+    public void onButtonClick(View view){
+    finish();
+    }
+}
